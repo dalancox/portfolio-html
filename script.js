eb41c39fb3e31fcc60aa46dbf045067c20c39d27
Jamie Dalan Cox <74336097+dalancox@users.noreply.github.com>
2023-01-27 13:33:57 -0700

    added a coursel to the about section

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 //JavaScript to handle anchor link clicks
 const anchorLinks = document.querySelectorAll("a[href^='#']");
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-back");
+const nextButton = document.getElementById("slide-arrow-forward");
+
 anchorLinks.forEach((link) => {
   link.addEventListener("click", (event) => {
     event.preventDefault(); // prevent the default anchor link behavior
     const targetId = event.target.getAttribute("href");
     document.querySelector(targetId).scrollIntoView({ behavior: "smooth" });
   });
+});
+
+nextButton.addEventListener("click", (event) => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
 });
